8203fadc99ba7999d30aa3c12b3a13ccd3679cf4
olehone <115097045+olehone@users.noreply.github.com>
2024-10-02 23:52:40 +0300

    change structure

--- init.py
+++
@@ -1,7 +0,0 @@
-memory = 0
-history = []
-available_operations = ["+", "-", "*", "/", "^", "√", "sqrt", "%"]
-unary_operations = ["√", "sqrt"]
-double_operations = ["+", "-", "*", "/", "^", "%"]
-decimals = 2
-log_file = "./logs/calc_error.log"
